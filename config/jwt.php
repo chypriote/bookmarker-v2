c5c8ae2d55ec19ceb9480f81bba9152af0c1b025
Nicolas <nicolastem@hotmail.fr>
2017-08-15 17:36:28 +0200

    small changes

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'sbX9OPex5YaqgVDj1bhXRTrM1mMcsHZi'),
 
     /*
     |--------------------------------------------------------------------------
